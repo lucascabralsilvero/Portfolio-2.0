3af528a1facadedf0894f48358c6783b4de3f2c9
Lucas Cabral <lucascabral6008@gmail.com>
2022-12-27 14:35:53 -0300

    adding items home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,19 +3,27 @@ import photo from "../assets/image.png";
 
 const Home = () => {
   return (
-    <div className=" text-center pt-24 min-h-screen bg-slate-400 dark:bg-slate-900 flex justify-center items-center dark:text-white">
-      <div>
-        <span>Hola!</span>
-        <h1 className=" ">Soy <span classname="font-bold">Lucas</span> </h1>
-        <h2 className="text-2xl">Desarrollador Web</h2>
-        <button className="">Descargar CV</button>
+    <div className=" text-center pt-24 min-h-screen bg-slate-400 dark:bg-slate-900  dark:text-white">
+      <div classname="flex flex-col justify-around items-center">
+        <div>
+        <span>Hola, soy</span>
+          <h1 className=" text-3xl ">
+            LUCAS CABRAL
+          </h1>
+          <h2 className="">Desarrollador Web</h2>
+         
+        </div>
+
+        <div>
+          <img
+            className=" w-96 border-4 border-white rounded-full  "
+            src={photo}
+            alt="photo"
+          />
+        </div>
       </div>
       <div>
-        <img
-          className=" w-96 border-4 border-white rounded-full  "
-          src={photo}
-          alt="photo"
-        />
+        <button>Descargar CV</button>
       </div>
     </div>
   );
